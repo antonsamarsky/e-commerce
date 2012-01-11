753359fb3daee7b02a540abcd532a471b9a321dc
asa <asa@e-conomic.com>
2012-01-11 12:21:55 +0200

    Move ValidatePasswordLengthAttribute to the separate file and namespace.

--- Bikee/Bikee.Web/Models/AccountModels.cs
+++ Bikee/Bikee.Web/Models/AccountModels.cs
@@ -1,9 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Globalization;
 using System.Web.Mvc;
 using System.Web.Security;
+using Bikee.Web.Models.ValidationAttributes;
 
 namespace Bikee.Web.Models
 {
@@ -87,7 +86,8 @@ namespace Bikee.Web.Models
 	{
 		private readonly MembershipProvider _provider;
 
-		public AccountMembershipService() : this(null)
+		public AccountMembershipService()
+			: this(null)
 		{
 		}
 
@@ -134,7 +134,7 @@ namespace Bikee.Web.Models
 			try
 			{
 				MembershipUser currentUser = _provider.GetUser(userName, true /* userIsOnline */);
-				return currentUser.ChangePassword(oldPassword, newPassword);
+				return currentUser != null && currentUser.ChangePassword(oldPassword, newPassword);
 			}
 			catch (ArgumentException)
 			{
@@ -208,35 +208,6 @@ namespace Bikee.Web.Models
 				default:
 					return "An unknown error occurred. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
 			}
-		}
-	}
-
-	[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
-	public sealed class ValidatePasswordLengthAttribute : ValidationAttribute, IClientValidatable
-	{
-		private const string _defaultErrorMessage = "'{0}' must be at least {1} characters long.";
-		private readonly int _minCharacters = Membership.Provider.MinRequiredPasswordLength;
-
-		public ValidatePasswordLengthAttribute()
-			: base(_defaultErrorMessage)
-		{
-		}
-
-		public override string FormatErrorMessage(string name)
-		{
-			return String.Format(CultureInfo.CurrentCulture, ErrorMessageString,
-					name, _minCharacters);
-		}
-
-		public override bool IsValid(object value)
-		{
-			string valueAsString = value as string;
-			return (valueAsString != null && valueAsString.Length >= _minCharacters);
-		}
-
-		public IEnumerable<ModelClientValidationRule> GetClientValidationRules(ModelMetadata metadata, ControllerContext context)
-		{
-			return new[] { new ModelClientValidationStringLengthRule(FormatErrorMessage(metadata.GetDisplayName()), _minCharacters, int.MaxValue) };
 		}
 	}
 	#endregion
---
+++ Bikee/Bikee.Web/Models/ValidationAttributes/ValidatePasswordLengthAttribute.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Bikee.Web.Models.ValidationAttributes
+{
+	[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+	public sealed class ValidatePasswordLengthAttribute : ValidationAttribute, IClientValidatable
+	{
+		private static readonly string DefaultErrorMessage = "'{0}' must be at least {1} characters long.";
+		private readonly int minCharacters = Membership.Provider.MinRequiredPasswordLength;
+
+		public ValidatePasswordLengthAttribute(): base(DefaultErrorMessage)
+		{
+		}
+
+		public override string FormatErrorMessage(string name)
+		{
+			return String.Format(CultureInfo.CurrentCulture, ErrorMessageString, name, this.minCharacters);
+		}
+
+		public override bool IsValid(object value)
+		{
+			var valueAsString = value as string;
+			return (valueAsString != null && valueAsString.Length >= this.minCharacters);
+		}
+
+		public IEnumerable<ModelClientValidationRule> GetClientValidationRules(ModelMetadata metadata, ControllerContext context)
+		{
+			return new[]
+								{
+									new ModelClientValidationStringLengthRule(FormatErrorMessage(metadata.GetDisplayName()), this.minCharacters, int.MaxValue)
+								};
+		}
+	}
+}
